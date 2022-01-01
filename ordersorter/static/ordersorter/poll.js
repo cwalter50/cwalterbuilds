b9d9aa2c232620fa89ae25d11e994e8c10b90c71
Chris Walter <walt50@gmail.com>
2022-01-01 10:54:23 -0500

    added touch gestures to ordersorter

--- ordersorter/static/ordersorter/poll.js
+++ ordersorter/static/ordersorter/poll.js
@@ -10,7 +10,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Query all items
     [].slice.call(list.querySelectorAll('.draggable')).forEach(function(item) {
-        item.addEventListener('mousedown', mouseDownHandler);
+        // item.addEventListener('mousedown', mouseDownHandler);
+        item.addEventListener('touchstart', touchStartHandler, false);
+
+//         someElement.addEventListener('touchstart', process_touchstart, false);
+// someElement.addEventListener('touchmove', process_touchmove, false);
+// someElement.addEventListener('touchcancel', process_touchcancel, false);
+// someElement.addEventListener('touchend', process_touchend, false);
     });
 
 });
@@ -40,6 +46,21 @@ const mouseDownHandler = function(e) {
     document.addEventListener('mouseup', mouseUpHandler);
 };
 
+const touchStartHandler = function(e) {
+    draggingEle = e.target;
+    draggingEle.style.backgroundColor = 'Gray';
+
+    // Calculate the mouse position
+    const rect = draggingEle.getBoundingClientRect();
+    // x = e.pageX - rect.left;
+    x = e.pageX;
+    y = e.pageY-rect.top-window.scrollY;
+
+    // Attach the listeners to `document`
+    document.addEventListener('touchmove', touchMoveHandler);
+    document.addEventListener('touchend', touchEndHandler);
+};
+
 const swap = function(nodeA, nodeB) {
     const parentA = nodeA.parentNode;
     const siblingA = nodeA.nextSibling === nodeB ? nodeA : nodeA.nextSibling;
@@ -49,6 +70,81 @@ const swap = function(nodeA, nodeB) {
 
     // Move `nodeB` to before the sibling of `nodeA`
     parentA.insertBefore(nodeB, siblingA);
+};
+
+const touchMoveHandler = function(e) {
+
+    const draggingRect = draggingEle.getBoundingClientRect();
+
+    if (!isDraggingStarted) {
+        // Update the flag
+        isDraggingStarted = true;
+        
+        // Let the placeholder take the height of dragging element
+        // So the next element won't move up
+        placeholder = document.createElement('div');
+        placeholder.classList.add('placeholder');
+
+        draggingEle.parentNode.insertBefore(
+            placeholder,
+            draggingEle.nextSibling
+        );
+
+        // Set the placeholder's height
+        placeholder.style.height = `${draggingRect.height}px`;
+    }
+    // Set position for dragging element
+    draggingEle.style.position = 'absolute';
+    draggingEle.style.top = `${e.pageY - y}px`; 
+    draggingEle.style.left = `${e.pageX - x}px`;
+
+    // The current order:
+    // prevEle
+    // draggingEle
+    // placeholder
+    // nextEle
+    const prevEle = draggingEle.previousElementSibling;
+    const nextEle = placeholder.nextElementSibling;   
+    
+    // User moves item to the top
+    if (prevEle && isAbove(draggingEle, prevEle)) {
+        // The current order    -> The new order
+        // prevEle              -> placeholder
+        // draggingEle          -> draggingEle
+        // placeholder          -> prevEle
+        swap(placeholder, draggingEle);
+        swap(placeholder, prevEle);
+        return;
+    }
+    // User moves the dragging element to the bottom
+    if (nextEle && isAbove(nextEle, draggingEle)) {
+        // The current order    -> The new order
+        // draggingEle          -> nextEle
+        // placeholder          -> placeholder
+        // nextEle              -> draggingEle
+        swap(nextEle, placeholder);
+        swap(nextEle, draggingEle);
+    }
+};
+
+const touchEndHandler = function() {
+    // Remove the placeholder
+    placeholder && placeholder.parentNode.removeChild(placeholder);
+    // Reset the flag
+    isDraggingStarted = false;
+    // Remove the position styles
+    draggingEle.style.removeProperty('top');
+    draggingEle.style.removeProperty('left');
+    draggingEle.style.removeProperty('position');
+    draggingEle.style.removeProperty('background-color');
+
+    x = null;
+    y = null;
+    draggingEle = null;
+
+    // Remove the handlers of `mousemove` and `mouseup`
+    document.removeEventListener('touchmove', touchMoveHandler);
+    document.removeEventListener('touchend', touchEndHandler);
 };
 
 
